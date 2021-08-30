65bbfb1d621c5df25ebeb618d087afcc58a76e53
laptop1 <laptop1@gmail.com>
2021-08-30 15:42:20 -0400

    Microservices shoppingMicroservices ShoppingCart: DiscountGrpc - Entities and Repositories

--- src/Microservices_ShoppingCart/Services/Discount/Discount.Grpc/Startup.cs
+++ src/Microservices_ShoppingCart/Services/Discount/Discount.Grpc/Startup.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Discount.Grpc.Repositories;
+using Discount.Grpc.Repositories.Interface;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Discount.Grpc
 {
@@ -17,6 +15,8 @@ namespace Discount.Grpc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
