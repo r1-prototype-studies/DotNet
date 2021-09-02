882761da883130f51fd4da3864ee7ee6c0e2f0a6
laptop1 <laptop1@gmail.com>
2021-09-02 12:38:21 -0400

    Microservices ShoppingCart: OrderApi- testing fixes

--- src/Microservices_ShoppingCart/Services/Ordering/Ordering.Application/Behaviors/ValidationBehaviour.cs
+++ src/Microservices_ShoppingCart/Services/Ordering/Ordering.Application/Behaviors/ValidationBehaviour.cs
@@ -10,10 +10,11 @@ using ValidationException = Ordering.Application.Exceptions.ValidationException;
 namespace Ordering.Application.Behaviors
 {
     public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+                                                     where TRequest : IRequest<TResponse>
     {
-        private readonly IEnumerable<IValidator<IRequest>> _validators;
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
 
-        public ValidationBehavior(IEnumerable<IValidator<IRequest>> validators)
+        public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
         {
             _validators = validators ?? throw new ArgumentNullException(nameof(validators));
         }
