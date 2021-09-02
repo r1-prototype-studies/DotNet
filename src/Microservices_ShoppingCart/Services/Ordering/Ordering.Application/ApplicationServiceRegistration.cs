c4f50590ff35c4526907323221f504d6cab81d66
laptop1 <laptop1@gmail.com>
2021-09-02 09:11:58 -0400

    Microservices ShoppingCart: OrderApi- Services Registration

--- src/Microservices_ShoppingCart/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
+++ src/Microservices_ShoppingCart/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -8,7 +8,7 @@ namespace Ordering.Application
 {
     public static class ApplicationServiceRegistration
     {
-        private static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
