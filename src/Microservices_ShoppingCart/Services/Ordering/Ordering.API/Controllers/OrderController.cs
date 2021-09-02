9b95a78564478ae242d5dd381779bb3aeb1462d5
laptop1 <laptop1@gmail.com>
2021-09-01 20:42:32 -0400

    Microservices ShoppingCart: OrderApi- Api Implementation

---
+++ src/Microservices_ShoppingCart/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,68 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CheckOutOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ordering.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        // GET api/<OrderController>/5
+        [HttpGet("{userName}", Name = "GetOrder")]
+        [ProducesResponseType(typeof(IEnumerable<OrdersVm>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrdersVm>>> GetOrdersByUserName(string userName)
+        {
+            var query = new GetOrdersListQuery(userName);
+            var orders = await _mediator.Send(query);
+            return Ok(orders);
+        }
+
+        // POST api/<OrderController>
+        [HttpPost(Name = "CheckoutOrder")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<ActionResult<int>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        // PUT api/<OrderController>/5
+        [HttpPut(Name = "UpdateOrder")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> Put([FromBody] UpdateOrderCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        // DELETE api/<OrderController>/5
+        [HttpDelete("{id}", Name = "DeleteOrder")]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var command = new DeleteOrderCommand { Id = id };
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
